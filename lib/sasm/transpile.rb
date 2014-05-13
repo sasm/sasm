f50eb7e08d26def30dce514a63e2df0def813534
ArchimedesPi <archimedespi3141@gmail.com>
2014-05-13 14:49:04 -0500

    Got some tests * properly* failing

--- lib/sasm/transpile.rb
+++ lib/sasm/transpile.rb
@@ -1,5 +1,13 @@
 module Sasm
   class Transpiler
     # Do nothing ;)
+    def initialize(source)
+      @source = source
+    end
+    
+    def process
+      # Return source *verbatim*
+      return @source
+    end
   end
 end
